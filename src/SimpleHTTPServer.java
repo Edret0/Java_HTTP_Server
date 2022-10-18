73e244ad5ed5a7acff5d0edec910ef3e3c705c3f
Edret0 <Josejaviernavarro1@gmail.com>
2022-10-18 17:21:51 -0400

    Added project config files and changed the .write method from the output stream to use StandardCharsets.UTF_8

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -1,14 +1,13 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 public class SimpleHTTPServer{
     
     public static void main(String[] args) throws IOException {
         /*
-         * Java program which creates a HTTP server that allows the user to connect to 
+         * Java program which creates an HTTP server that allows the user to connect to
          */
         final ServerSocket server = new ServerSocket(8000);
         System.out.println("Listening for connection on port 8000....");
@@ -25,7 +24,7 @@ public class SimpleHTTPServer{
                 Date today = new Date();
                 String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
                 client.getOutputStream()
-                .write(httpResponse.getBytes("UTF-8"));
+                .write(httpResponse.getBytes(StandardCharsets.UTF_8));
              } catch (Exception e) {
                 // TODO: handle exception
              }
